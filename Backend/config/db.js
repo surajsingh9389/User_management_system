314ea7f845cd3b39db5204149a81af3421caa451
surajsingh9389 <sk9389623488@gmail.com>
2026-02-22 11:55:44 +0530

    mongo uri updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Function to connect MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/userManagementSystem`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
